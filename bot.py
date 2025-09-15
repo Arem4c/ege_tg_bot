7bf1dc3cf7ffbca0d1b068a068db2631e8eab83c
Artem Ivanov <ivanov.sunburn@gmail.com>
2025-09-15 14:29:19 +0300

    загрузка задание по предметам

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 from telebot import types, TeleBot
-from fipi import load_subjects
+from fipi import load_subjects, load_tasks
 
 bot = TeleBot('8438991402:AAH2kNsDOpM5xCN1lNYDS3MoHbeJEGZQQJ0')
 
@@ -27,30 +27,32 @@ def get_text_messages(message):
 
 @bot.callback_query_handler(func=lambda callback: True)
 def callback_message(callback):
-    buttons = types.InlineKeyboardMarkup()
-    global question_number
-    if callback.data == 'theme1':
-        question_number += 1
-        buttons.add(types.InlineKeyboardButton('Ответ', parse_mode='html', reply_markup=buttons, callback_data=f'ans{question_number}'))
-        bot.send_message(callback.message.chat.id, f'Задание 1: {zadanie1[0][0]}'.format(callback.message.from_user), reply_markup=buttons)
-        # bot.delete_message(callback.message.chat.id, callback.message.message_id)
-    if callback.data == f'ans{question_number}':
-        if question_number < 4:
-            buttons.add(types.InlineKeyboardButton('Далее', parse_mode='html', reply_markup=buttons, callback_data=f'next{question_number}'))
-            bot.send_message(callback.message.chat.id, f'Ответ: {zadanie1[question_number][1]}'.format(callback.message.from_user), reply_markup=buttons)
-        if question_number == 4:
-            buttons.add(types.InlineKeyboardButton('Назад к выбору темы', parse_mode='html', reply_markup=buttons, callback_data='back'))
-            bot.send_message(callback.message.chat.id, f'Ответ: {zadanie1[question_number][1]}'.format(callback.message.from_user), reply_markup=buttons)
-        # bot.delete_message(callback.message.chat.id, callback.message.message_id)
-    if callback.data == f'next{question_number}':
-        question_number += 1
-        buttons.add(types.InlineKeyboardButton('Ответ', parse_mode='html', reply_markup=buttons, callback_data=f'ans{question_number}'))
-        bot.send_message(callback.message.chat.id, f'Задание {question_number+1}: {zadanie1[question_number][0]}'.format(callback.message.from_user), reply_markup=buttons)
-        # bot.delete_message(callback.message.chat.id, callback.message.message_id)
-    if callback.data == 'back':
-        question_number = -1
-        buttons = types.InlineKeyboardMarkup()
-        buttons.add(types.InlineKeyboardButton('Тема 1', parse_mode='html', reply_markup=buttons, callback_data='theme1'))
-        bot.send_message(callback.message.chat.id, "Выбери тему".format(callback.message.from_user), reply_markup=buttons)
 
+    bot.send_message(callback.message.chat.id, load_tasks(callback.data))
+    # buttons = types.InlineKeyboardMarkup()
+    # global question_number
+    # if callback.data == 'theme1':
+    #     question_number += 1
+    #     buttons.add(types.InlineKeyboardButton('Ответ', parse_mode='html', reply_markup=buttons, callback_data=f'ans{question_number}'))
+    #     bot.send_message(callback.message.chat.id, f'Задание 1: {zadanie1[0][0]}'.format(callback.message.from_user), reply_markup=buttons)
+    #     # bot.delete_message(callback.message.chat.id, callback.message.message_id)
+    # if callback.data == f'ans{question_number}':
+    #     if question_number < 4:
+    #         buttons.add(types.InlineKeyboardButton('Далее', parse_mode='html', reply_markup=buttons, callback_data=f'next{question_number}'))
+    #         bot.send_message(callback.message.chat.id, f'Ответ: {zadanie1[question_number][1]}'.format(callback.message.from_user), reply_markup=buttons)
+    #     if question_number == 4:
+    #         buttons.add(types.InlineKeyboardButton('Назад к выбору темы', parse_mode='html', reply_markup=buttons, callback_data='back'))
+    #         bot.send_message(callback.message.chat.id, f'Ответ: {zadanie1[question_number][1]}'.format(callback.message.from_user), reply_markup=buttons)
+    #     # bot.delete_message(callback.message.chat.id, callback.message.message_id)
+    # if callback.data == f'next{question_number}':
+    #     question_number += 1
+    #     buttons.add(types.InlineKeyboardButton('Ответ', parse_mode='html', reply_markup=buttons, callback_data=f'ans{question_number}'))
+    #     bot.send_message(callback.message.chat.id, f'Задание {question_number+1}: {zadanie1[question_number][0]}'.format(callback.message.from_user), reply_markup=buttons)
+    #     # bot.delete_message(callback.message.chat.id, callback.message.message_id)
+    # if callback.data == 'back':
+    #     question_number = -1
+    #     buttons = types.InlineKeyboardMarkup()
+    #     buttons.add(types.InlineKeyboardButton('Тема 1', parse_mode='html', reply_markup=buttons, callback_data='theme1'))
+    #     bot.send_message(callback.message.chat.id, "Выбери тему".format(callback.message.from_user), reply_markup=buttons)
+    #
 bot.polling(none_stop=True, interval=0)
---
+++ fipi.py
@@ -0,0 +1,51 @@
+import requests
+import urllib3
+from bs4 import BeautifulSoup
+from urllib3.exceptions import InsecureRequestWarning
+
+urllib3.disable_warnings(InsecureRequestWarning)
+
+URL_SUBJECTS = "https://ege.fipi.ru/bank/"
+URL_TASKS = "https://ege.fipi.ru/bank/questions.php?proj="
+
+def load_subjects():
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
+    }
+    response = requests.get(URL_SUBJECTS, verify=False, headers=headers)
+    soup = BeautifulSoup(response.text, "lxml")
+    subjects = []
+    for subject in soup.find_all("li"):
+        onclick = subject["onclick"]
+        index = onclick.rfind('"')
+        c = subject["onclick"][15:index]
+        subjects += [{"title": subject.text, "data": c}]
+    return subjects
+
+def load_tasks(project):
+    h = ''
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
+    }
+    response = requests.get(URL_TASKS + project + "&page=1&pagesize=1", verify=False, headers=headers)
+    soup = BeautifulSoup(response.text, "lxml")
+    tasks = soup.find_all('div', class_='qblock')
+    for task in tasks:
+        b = task.find_all('p', class_='MsoNormal')
+        task_text = task.text.split("\n")
+        # print(len(task_text))
+
+        for i in task_text:
+            n_i = i.strip()
+            if len(n_i) != 0:
+                h += n_i + "\n"
+        # print(task.text)
+        # for p in b:
+        #     print(p.text)
+        # break
+    return h
+
+
+
+
+load_tasks("B9ACA5BBB2E19E434CD6BEC25284C67F")
